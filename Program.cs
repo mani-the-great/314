fb6cd7ac285295e1db4df9e2b98ce6a69a24b124
Alireza <alireza.haghighi1385@gmail.com>
2025-07-06 20:30:38 +0330

    feat: add required migration

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.ConfigureApplicationCookie(options =>
